9e53b0e925007d3dfda65b9ce2ed113dbdc6f62c
Masih Tabrizi <masih_tabrizi@softwaremerchant.com>
2015-02-06 15:07:29 -0500

    Revert 6702bc1..5bc5e81
    This rolls back to commit 6702bc134e39604f4704e604c5597297901a43f8.

--- FederalTapHouse2/main.m
+++ FederalTapHouse2/main.m
@@ -5,9 +5,6 @@
 //  Created by Di Kong on 2/6/15.
 //  Copyright (c) 2015 Software Merchant. All rights reserved.
 //
-// This is forked by masiht
-// It is just a test!
-// This is another test for commiting
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
