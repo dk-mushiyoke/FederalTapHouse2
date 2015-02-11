f2ea581e64c7ea6abef7f222e8f02d283943e47d
Merritt Tidwell <merritt.tidwell@gmail.com>
2015-02-11 10:30:50 -0600

    changes segue on login

--- FederalTapHouse2/ViewControllers/FTHLogInViewController.m
+++ FederalTapHouse2/ViewControllers/FTHLogInViewController.m
@@ -76,14 +76,14 @@
                       otherButtonTitles:nil] show];
 }
 
-/*
+
  #pragma mark - Navigation
  
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
+// // In a storyboard-based application, you will often want to do a little preparation before navigation
+// - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+// // Get the new view controller using [segue
+//     // Pass the selected object to the new view controller.
+// }
+
 
 @end
